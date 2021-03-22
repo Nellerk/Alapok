d9786a47406a34e31a9cfc57f89ce927b246a9cc
Kristóf Neller <neller.kristof@outlook.com>
2021-03-22 11:27:56 +0100

    Proba

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alapok
+{
+    class Program
+    {
+        static void Main(string{ } args)
+        {
+            string nev = "Neller Kristof";
+
+    }
+}
